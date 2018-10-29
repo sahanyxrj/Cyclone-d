855c98c5de728e6c3f4c65fc91507d5e0875a79b
sahanyxrj <42677981+sahanyxrj@users.noreply.github.com>
2018-10-29 11:10:12 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,18 +2,19 @@ public class Main {
 
     public static void main(String[] args)
     {
-        double d1=calcD(25,15,23,20);
-        double d2=calcD(23,20,21,24);
-        double d3=calcD(21,24,20,27);
-        double d4=calcD(20,27,19,35);
-        double d5=calcD(19,35,21,46);
-        double d6=calcD(21,46,23,56);
-        double d7=calcD(23,56,25,65);
-        double d8=calcD(25,65,28,70);
-        double d9=calcD(28,70,32,73);
-        double d10=calcD(32,73,34,74);
-        double d11=calcD(34,74,36,74);
-        double d12=calcD(36,74,39,74);
+        double d1=calcD(Math.toRadians(25),Math.toRadians(-15),Math.toRadians(23),Math.toRadians(-20));
+        double d2=calcD(Math.toRadians(23),Math.toRadians(-20),Math.toRadians(21),Math.toRadians(-24));
+        double d3=calcD(Math.toRadians(21),Math.toRadians(-24),Math.toRadians(20),Math.toRadians(-27));
+        double d4=calcD(Math.toRadians(20),Math.toRadians(-27),Math.toRadians(19),Math.toRadians(-35));
+        double d5=calcD(Math.toRadians(19),Math.toRadians(-35),Math.toRadians(21),Math.toRadians(-46));
+        double d6=calcD(Math.toRadians(21),Math.toRadians(-46),Math.toRadians(23),Math.toRadians(-56));
+        double d7=calcD(Math.toRadians(23),Math.toRadians(-56),Math.toRadians(25),Math.toRadians(-65));
+        double d8=calcD(Math.toRadians(25),Math.toRadians(-65),Math.toRadians(28),Math.toRadians(-70));
+        double d9=calcD(Math.toRadians(28),Math.toRadians(-70),Math.toRadians(32),Math.toRadians(-73));
+        double d10=calcD(Math.toRadians(32),Math.toRadians(-73),Math.toRadians(34),Math.toRadians(-74));
+        double d11=calcD(Math.toRadians(34),Math.toRadians(-74),Math.toRadians(36),Math.toRadians(-74));
+        double d12=calcD(Math.toRadians(36),Math.toRadians(-74),Math.toRadians(39),Math.toRadians(-74));
+
 
         double totalDistance=d1+d2+d3+d4+d5+d6+d7+d8+d9+d10+d11+d12;
         System.out.printf("Total Distance is %,.2f km.",totalDistance);
@@ -21,6 +22,11 @@ public class Main {
 
     public static double calcD(double x1, double y1, double x2, double y2)
     {
+        double latitude1=Math.toRadians(x1);
+        double latitude2=Math.toRadians(x2);
+        double longitude1=Math.toRadians(y1);
+        double longitude2=Math.toRadians(y2);
+
         double a= Math.sin((x1-x2)/2)*Math.sin((x1-x2)/2)+ Math.cos(x1)* Math.cos(x2)* Math.sin((y1-y2)/2)*Math.sin((y1-y2)/2);
         double c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
         double R = 6371;
